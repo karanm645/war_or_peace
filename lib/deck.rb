ed184343d72f19520d36b6839e21cef09df3294d
Karan Mehta <79548116+karanm645@users.noreply.github.com>
2023-07-01 13:03:33 -0500

    "finish iteration 1"

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,11 +1,60 @@
 class Deck 
-  attr_reader :cards_array
+  attr_reader :cards_array,
+              :high_ranking_cards
 
   def initialize(cards_array)
     @cards_array = cards_array 
+    @high_ranking_cards = []
   end 
 
   def rank_of_card_at(index)
     @cards_array[index].rank
+  end 
+
+  def high_ranking_cards
+    # use select method to easily pick
+    @cards_array.select do |card|
+      card.rank >= 11
+    end 
+    
+    # iterate through cards and select the ones that have 11 or <
+    # @cards_array.each do |card|
+    #   if card.rank >= 11 
+    #     @high_ranking_cards <<  card 
+    #   end 
+    # end 
+    # @high_ranking_cards
+  end 
+
+  def percent_high_ranking
+    # 2(high rank) / 3 (cards array)
+    all_rank = []
+    high = 0
+    low = 0 
+
+    @cards_array.each do |card|
+      all_rank << card.rank 
+    end 
+
+    all_rank.each do |i|
+      if i >= 11
+        high += 1
+      else 
+        low += 1
+      end 
+    end 
+
+    #percentage = high cards / all cards
+    percentage = (high.to_f / @cards_array.count.to_f) * 100
+    percentage.round(2)
+  end 
+
+  def add_card(card)
+    @cards_array << card 
+  end 
+  def removed_card
+    #remove top card --> shift + 1st one
+    first_card = @cards_array.shift(1)
+    return first_card.pop # remove from array
   end 
 end 
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -4,24 +4,45 @@ require './lib/deck'
 require 'pry'
 
 RSpec.describe Deck do
+  before(:each) do 
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3)
+    @card3 = Card.new(:heart, 'Ace', 14)
+    @cards_array = [@card1, @card2, @card3]
+    @deck = Deck.new(@cards_array)
+  end 
   it "has an array of cards" do
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards_array = [card1, card2, card3]
-    deck = Deck.new(cards_array)
-# binding.pry
-    expect(deck.cards_array).to eq(cards_array)
+
+    expect(@deck.cards_array).to eq(@cards_array)
   end 
 
   it "can find rank by indexing" do 
-        card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards_array = [card1, card2, card3]
-    deck = Deck.new(cards_array)
-# binding.pry
-    expect(deck.rank_of_card_at(0)).to eq(12)
-    expect(deck.rank_of_card_at(2)).to eq(14)
+
+    expect(@deck.rank_of_card_at(0)).to eq(12)
+    expect(@deck.rank_of_card_at(2)).to eq(14)
+  end 
+
+  it "can calculate the high ranking cards of rank 11 <" do 
+    expect(@deck.high_ranking_cards).to eq([@card1, @card3])
+  end 
+
+  it "can calculate the percentage of cards that are high ranking" do 
+  
+    expect(@deck.percent_high_ranking).to eq(66.67)
+  end 
+  it "can remove and return a card from the deck" do
+    removed_card = @card1
+
+    expect(@deck.removed_card).to eq(removed_card)
+    expect(@deck.high_ranking_cards).to eq([@card3])
+    expect(@deck.percent_high_ranking).to eq(50.00)
+
+    card4 = Card.new(:club, '5', 5)
+
+    @deck.add_card(card4)
+
+    expect(@deck.cards_array).to eq([@card2, @card3, card4])
+    expect(@deck.high_ranking_cards).to eq([@card3])
+    expect(@deck.percent_high_ranking).to eq(33.33)
   end 
 end 
